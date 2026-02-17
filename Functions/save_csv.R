ffce7e2b116bedbfa1a238263fcbf9766377c6ec
Abdalla Taha <53947286+abdallataha@users.noreply.github.com>
2026-02-18 00:06:28 +0100

    Update save_csv.R

--- Functions/save_csv.R
+++ Functions/save_csv.R
@@ -3,11 +3,12 @@ save_csv <- function(dataframe, file) {
   local_path <- "F:/Digital Health Master's Potsdam/1st Semester DH/Fundmentals of Programming R/Project/"
   
   # Combining the path and the filename
-  full_path <- paste0(local_path, filename)
+  full_path <- paste0(local_path, file)
   
   # File saving
   write.csv(dataframe, file = full_path, row.names = FALSE)
   
   # Show me a confirmation message
   message("File successfully saved to: ", full_path)
+
 }
