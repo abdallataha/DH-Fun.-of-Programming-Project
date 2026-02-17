2f68e82564ce62275fc572e43417c5b656ecd86b
Abdalla Taha <53947286+abdallataha@users.noreply.github.com>
2026-02-17 23:53:21 +0100

    Add files via upload

--- Functions/save_csv.R
+++ Functions/save_csv.R
@@ -1,1 +1,13 @@
-
+save_csv <- function(dataframe, file) {
+  # Defining the folder where your project is on my computer
+  local_path <- "F:/Digital Health Master's Potsdam/1st Semester DH/Fundmentals of Programming R/Project/"
+  
+  # Combining the path and the filename
+  full_path <- paste0(local_path, filename)
+  
+  # File saving
+  write.csv(dataframe, file = full_path, row.names = FALSE)
+  
+  # Show me a confirmation message
+  message("File successfully saved to: ", full_path)
+}
