ab342ceb870f7ed792e54c0816bc868d4ce6a939
Abdalla <dr.abdullah_taha@yahoo.com>
2026-02-19 00:32:01 +0100

    - Adding visualizations. - Adding visual function to automate the basic 4 types of visualizations. - Adding the starting dataset for visualizations.

---
+++ Functions/visual.R
@@ -0,0 +1,20 @@
+visual <- function(type, x, y = NULL, title = "", xlab = "", ylab = "", col = "lightblue") {
+  # Setting the margins
+  par(mgp = c(4, 1, 0), mar = c(7, 6, 4, 2))
+
+  if (type == "bar") {
+    bp<- barplot(x, main = title, xlab = xlab, ylab = ylab, col = col, las = 2, ylim = c(0, max(x)*1.1))
+    text(x = bp, y = x, labels = x, pos = 3, col = "black")
+  } else if (type == "hist") 
+    {
+    hp<- hist(x, main = title, xlab = xlab, col = col)
+    text(x = hp, y = x, labels = x, pos = 3, col = "black")
+  } else if (type == "scatter") 
+    {
+    plot(x, y, main = title, xlab = xlab, ylab = ylab, col = col, pch = 16)
+  } else if (type == "box") 
+    {
+    boxplot(x ~ y, main = title, xlab = xlab, ylab = ylab, col = col, las = 2)
+  }
+
+}
