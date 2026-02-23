063dfbb843f2a4562d2e4446223fc34b18b7ab85
Abdalla <dr.abdullah_taha@yahoo.com>
2026-02-23 02:33:24 +0100

    visual function edit (par removed)

--- Functions/visual.R
+++ Functions/visual.R
@@ -1,22 +1,18 @@
 visual <- function(type, x, y = NULL, title = "", xlab = "", ylab = "", 
                    col = "lightblue", ...) {
-  # Setting the margins
-  par(mgp = c(4, 1, 0), mar = c(7, 6, 4, 2))
 
   if (type == "bar") {
-    bp<- barplot(x, main = title, xlab = xlab, ylab = ylab, col = col, 
-                 ylim = c(0, max(x)*1.1), las = las, ...)
-    text(x = bp, y = x, labels = x, pos = 3, col = "black", las = las, ...)
-  } else if (type == "hist") 
-    {
-    hp<- hist(x, main = title, xlab = xlab, col = col, ...)
-    text(x = hp, y = x, labels = x, pos = 3, col = "black", ...)
-  } else if (type == "scatter") 
-    {
-    plot(x, y, main = title, xlab = xlab, ylab = ylab, col = col, pch = 16, ...)
-  } else if (type == "box") 
-    {
-    boxplot(x ~ y, main = title, xlab = xlab, ylab = ylab, col = col, las = 2, ...)
+    bp <- barplot(x, main = title, xlab = xlab, ylab = ylab, col = col, 
+                  ylim = c(0, max(x, na.rm = TRUE) * 1.2), ...)
+    text(x = bp, y = x, labels = x, pos = 3, col = "black")
+    
+  } else if (type == "hist") {
+    hist(x, main = title, xlab = xlab, col = col, ...)
+    
+  } else if (type == "scatter") {
+    plot(x, y, main = title, xlab = xlab, ylab = ylab, col = col, ...)
+    
+  } else if (type == "box") {
+    boxplot(x ~ y, main = title, xlab = xlab, ylab = ylab, col = col, ...)
   }
-
 }
